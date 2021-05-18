185518331eaae6b7881cab18a90a99dffc44f6c9
saikrishna <saikrishna31120@gmail.com>
2021-05-18 22:44:21 +0530

    fixed typos and small errors

--- box_class.py
+++ box_class.py
@@ -1,8 +1,6 @@
 import numpy as np
 import math
 from numpy.random import default_rng
-from matplotlib import pyplot as plt
-from matplotlib import animation, rc
 #from IPython.display import HTML
 rg = default_rng(1234)
 
@@ -14,10 +12,10 @@ class Box:
         self.pos = pos
         self.num = 0
         self.p_xy = np.empty((2,0),float)
-        self.p_state = np.empty((1,0),int)
+        self.p_state = np.empty(0,int)
         self.p_dest = np.empty((2,0),float)
         self.now = 0
-        self.time_inf = np.empty((2,0),float)
+        self.time_inf = np.empty(0,float)
         self.inf_c = 0
         self.sus_c = 0
         self.rec_c = 0
@@ -50,29 +48,32 @@ class Box:
                 i += 1
         self.num = self.num + num
         self.p_xy = np.append(self.p_xy,[x_list,y_list],axis=1)
-        self.p_state = np.append(self.p_xy,state_l,axis=1)
+        self.p_state = np.append(self.p_state,state_l)
+        self.time_inf= np.append(self.time_inf,inf_time)
     
     def move_people(self, displacement):
         bounce = lambda x, l, u:u - abs(u-l-x % (2*(u-l)))
         self.p_xy[0] = bounce(self.p_xy[0] + displacement[0],self.pos[0],self.pos[0]+self.size[0])
         self.p_xy[1] = bounce(self.p_xy[1] + displacement[1],self.pos[1],self.pos[1]+self.size[1]) 
         self.now = self.now + 0.1
-    
+
     def update_infection(self,rad):
-        inf_id = np.where(self.p_state == 1)
-        sus_id = np.where(self.p_state == 0)
+        inf_id, = np.where(self.p_state == 1)
+        sus_id, = np.where(self.p_state == 0)
         cur_inf=[]
         for i in inf_id:
             x_cent = self.p_xy[0][i]
             y_cent = self.p_xy[1][i]
             for j in sus_id:
-                if self.p_xy[0][j]>=x_cent-rad and self.p_xy[0][j] <= x_cent+rad and self.p_xy[1][j]>=y_cent-rad and self.p_xy[1][j]>=y_cent+rad:
+                if self.p_xy[0][j]>=x_cent-rad and self.p_xy[0][j] <= x_cent+rad and self.p_xy[1][j]>=y_cent-rad and self.p_xy[1][j]<=y_cent+rad:
                     if (self.p_xy[0][j] - x_cent)**2 + (self.p_xy[1][j] - y_cent)**2 <= rad**2:
-                        cur_inf.append(j)
+                        if j not in cur_inf:
+                            cur_inf.append(j)
         for i in cur_inf:
-            self.p_state[i]=rg.choice([0,1],p=[0.4,0.6]) #probability of infection
+            self.p_state[i]=rg.choice([0,1],p=[0.8,0.2]) #probability of infection
             if self.p_state[i] == 1:
                 self.time_inf[i]=self.now
+        self.counts()
     
     def set_normal_dest(self):
         dest_x = []
@@ -94,10 +95,14 @@ class Box:
         self.inf_c = freq[np.where(unique==1)]
         self.sus_c = freq[np.where(unique==0)]
         self.rec_c = freq[np.where(unique==-1)]
+        print(self.inf_c,self.sus_c,self.rec_c)
     
-    def update_recovered(self):
-        inf_id = np.where(self.p_state == 1)
+    def update_recovered(self,time):
+        inf_id, = np.where(self.p_state == 1)
         for i in inf_id:
-            if self.time_inf[i]-self.now>5: #time taken to recover-5 
-                self.p_state[i]==rg.choice([1,-1],p=[0.5,0.5]) #probability of recovery
-            
+            if self.now-self.time_inf[i]>=time: #time taken to recover 
+                self.p_state[i]=rg.choice([1,-1],p=[0.8,0.2]) #probability of recovery
+        self.counts()    
+
+    def print_val(self):
+        print(self.p_xy,self.p_state,self.p_dest)
